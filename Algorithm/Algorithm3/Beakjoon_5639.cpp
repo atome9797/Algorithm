f4ebdf8afe5a0bece271bb8c6aacb72fc2fe9cca
kimyounghoon <hungame@naver.com>
2022-07-01 07:39:41 +0900

    수정

---
+++ Algorithm/Algorithm3/Beakjoon_5639.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct node
+{
+	int left;
+	int right;
+};
+
+struct node root[1000001];//구조체 선언
+
+using namespace std;
+
+int main()
+{
+	//
+	//문제
+	// 1. 전위 순회한 결과값을 후위 순회의 결과로 바꾸어 출력하라
+	// 2. 뿌리 노드보다 작은 거는 왼쪽, 큰거는 오른쪽에 삽입하는 순서
+	//
+
+	int input = 0;
+
+	while (scanf("%d", &input) != EOF) {
+		root[input]
+	}
+
+	return 0;
+}
