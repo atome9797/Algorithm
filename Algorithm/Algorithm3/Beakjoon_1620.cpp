2d01d84dfa94a6be20e99d546355685e108dc5d6
kimyounghoon <hungame@naver.com>
2022-07-19 22:11:40 +0900

    220719 commit #1
    1) 1920 백준 집합 문제 풀이

--- Algorithm/Algorithm3/Beakjoon_1620.cpp
+++ Algorithm/Algorithm3/Beakjoon_1620.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <string>
 
 using namespace std;
 
@@ -23,20 +24,44 @@ int main()
 	int N, M;
 	cin >> N >> M;
 
-	unordered_map<string, int> map;
+	unordered_map<string, int> map1;
+	unordered_map<int, string> map2;
+	bool isNumber = false;
 	for (int i = 0; i < N + M; i++)
 	{
 		if (i < N)
 		{
 			string str = "";
 			cin >> str;
-			map.insert({ str, i});
+			map1.insert({ str, i + 1});
+			map2.insert({ i, str});
 		}
 		else
 		{
-			
 			string str = "";
 			cin >> str;
+
+			//입력 받는 게 문자인지 숫자인지 구분해줘야한다.
+			//유니코드로 구분한다.
+			if (str[0] >= '0' && str[0] <= '9')
+			{
+				isNumber = true;
+			}
+			else
+			{
+				isNumber = false;
+			}
+
+			if (isNumber)
+			{
+				int num = stoi(str);
+				cout << map2.at(num - 1) << "\n";
+			}
+			else
+			{
+				cout << map1.at(str) << "\n";
+			}
+
 		}
 	}
 
