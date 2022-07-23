61647b5d9f81b1fdadea113950cb949f51fe7472
kimyounghoon <hungame@naver.com>
2022-07-23 18:55:54 +0900

    220723 commit #1
    1) 정렬문제 추가

--- Algorithm/Algorithm3/Beakjoon_10989.cpp
+++ Algorithm/Algorithm3/Beakjoon_10989.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
-#include <queue>
-#include <algorithm>
 using namespace std;
+
+int setNum[10001] = { 0 };
 
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
+
+	
 	int N = 0;
 	cin >> N;
 	
-	priority_queue<int>pq;
 	for (int i = 0; i < N; i++)
 	{
 		int num = 0;
 		cin >> num;
 
-		if (!pq.empty() && num == 0)
-		{
-			cout << pq.top() << "\n";
-			pq.pop();
-		}
-		else if (pq.empty() && num == 0)
-		{
-			cout << 0 << "\n";
-		}
-		else if (num != 0)
-		{
-			pq.push(num);
-		}
+		setNum[num] += 1;
 	}
 
+	for (int i = 0; i < 10001; i++)
+	{
+		if (setNum[i] != 0)
+		{
+			cout << i << "\n";
+			setNum[i] -= 1;
+			i--;
+		}
+	}
+	
+	
 	return 0;
 }
